b681de05ec5fe0941b3ef9d475ded1fb054144d3
JoeCross <krstevskigj@gmail.com>
2024-02-20 19:19:34 +0000

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+function App() {
+  
+
+  return (
+    <div></div>
+  )
+}
+
+export default App
